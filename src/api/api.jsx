89ac60c935aad22970cc3f685e928ea0ecd0cfa4
Pawan Dasila <pawandasila06@gmail.com>
2025-03-29 09:32:30 +0530

    updated the api issue

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:4000/api";
+const BASE_URL = "https://yoga-backend-hy1v.vercel.app/api";
 
 export const SignUpUser = async (userData) => {
   try {
